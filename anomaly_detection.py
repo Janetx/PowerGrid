9cca4cce5fdf8e66ea1f624043deae92fca9899c
Rafay Tanzeel <rafaytanzeel@gmail.com>
2017-07-16 03:40:09 -0700

    basic project structure setup

---
+++ anomaly_detection.py
@@ -0,0 +1,37 @@
+import argparse, sys, os, logging
+import pandas as pd
+import powergrid_data
+
+parser = argparse.ArgumentParser(description='Anomaly Detection')
+parser.add_argument("-ts", "--test", dest="test", default=os.path.join("data", "test", "test_v1.csv"), help="File containing testing data (default=data/test)")
+parser.add_argument("-tr", "--train", dest="train", default=os.path.join("data", "train", "train.csv"), help="File containing training data (default=data/train)")
+parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Verbose mode (default=off)")
+args = parser.parse_args()
+
+
+def timeit(mainFunction):
+    import time
+    from functools import wraps
+
+    @wraps(mainFunction)
+    def function_wrapper(*args, **kwargs):
+        start = time.clock()
+        mainFunction(*args, **kwargs)
+        end = time.clock()
+        logging.debug("\nElapsed Time: %s \n" % (end - start,))
+
+    return function_wrapper
+
+
+@timeit
+def main():
+    power_grid = powergrid_data.datasets(args.train, args.test)
+    print power_grid.data #training data
+    print power_grid.target #testing data
+
+if __name__ == '__main__':
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    main()
+
---
+++ powergrid_data.py
@@ -0,0 +1,31 @@
+
+import os
+import logging
+import pandas as pd
+
+
+class datasets:
+    def __init__(self, train, test):
+        train_dir = os.path.dirname(train)
+        test_dir = os.path.dirname(test)
+
+        hdf_file = os.path.join("data", "hdf", "hdf_data.h5")
+
+        if not (os.path.isfile(hdf_file)):
+            logging.debug("Creating HDF5 Training and Testing Files")
+            train_data = pd.read_csv(train)
+            test_data = pd.read_csv(test)
+
+            train_data.to_hdf(hdf_file, "train_data", mode='w', format='table')
+            test_data.to_hdf(hdf_file, 'test_data', mode='a', format='table')
+
+        self._train_data = pd.read_hdf(hdf_file, "train_data")
+        self._test_data = pd.read_hdf(hdf_file, 'test_data')
+
+    @property
+    def data(self):
+        return self._train_data
+
+    @property
+    def target(self):
+        return self._test_data
