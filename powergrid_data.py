c6817549cfd974dc3e358394e3f26014520d3af9
Rafay Tanzeel <rafaytanzeel@gmail.com>
2017-07-23 08:35:46 -0700

    added powergrid analysis for may 2007 weekends

--- powergrid_data.py
+++ powergrid_data.py
@@ -10,20 +10,25 @@ class datasets:
         test_dir = os.path.dirname(test)
 
         train_data = pd.read_csv(train)
-        train_data.dropna()
+        
         train_data['DateTime'] = train_data['Date'] + ' ' + train_data['Time']
         train_data['Date'] = pd.to_datetime(train_data['Date'], format='%d/%m/%Y')
         train_data['Time'] = pd.to_datetime(train_data['Time'], format='%H:%M:%S')
         train_data['DateTime'] = pd.to_datetime(train_data['DateTime'], format='%d/%m/%Y %H:%M:%S')
+        train_data.dropna(inplace=True)
+        train_data.reset_index(drop=True, inplace=True)
         
-        
+		
+		
         test_data = pd.read_csv(test)
-        test_data.dropna()
+        
         test_data['DateTime'] = test_data['Date'] + ' ' + test_data['Time']
         test_data['Date'] = pd.to_datetime(test_data['Date'], format='%d/%m/%Y')
         test_data['Time'] = pd.to_datetime(test_data['Time'], format='%H:%M:%S')
         test_data['DateTime'] = pd.to_datetime(test_data['DateTime'], format='%d/%m/%Y %H:%M:%S')
-
+        test_data.dropna(inplace=True)
+        test_data.reset_index(drop=True, inplace=True)
+		
         self._train_data = train_data
         self._test_data = test_data
 
